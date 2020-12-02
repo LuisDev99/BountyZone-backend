4a1aa119761681a1dd09129d4d29ec1d30a058b5
Luis Gerardo <luisgpr99@gmail.com>
2020-12-02 14:53:17 -0600

    Create IEventLogService interface

---
+++ BountyZone.Core/Interfaces/Services/IEventLogService.cs
@@ -0,0 +1,12 @@
+﻿using BountyZone.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BountyZone.Core.Interfaces.Services
+{
+    public interface IEventLogService : IBaseService<EventLog>
+    {
+
+    }
+}
