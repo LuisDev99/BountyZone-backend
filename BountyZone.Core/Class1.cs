1c1d82fca430a52a740e899bb31fb3168c104bee
Luis Gerardo <luisgpr99@gmail.com>
2020-12-02 13:52:51 -0600

    Create BaseEntity and Player Entities

--- BountyZone.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace BountyZone.Core
-{
-    public class Class1
-    {
-    }
-}
---
+++ BountyZone.Core/Entities/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BountyZone.Core.Entities
+{
+    public class Player : BaseEntity
+    {
+        public string UserName { get; set; }
+
+        public string NickName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
