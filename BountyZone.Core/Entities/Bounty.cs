c8cba48a3515b38a9b3f021dd293eb41b734761e
Luis Gerardo <luisgpr99@gmail.com>
2020-12-02 14:11:45 -0600

    Add foreign relationships for Bounty Entity

--- BountyZone.Core/Entities/Bounty.cs
+++ BountyZone.Core/Entities/Bounty.cs
@@ -4,10 +4,25 @@ using System.Text;
 
 namespace BountyZone.Core.Entities
 {
-  public class Bounty : BaseEntity
-  {
-    public DateTime Time { get; set; }
+    public class Bounty : BaseEntity
+    {
+        public DateTime Time { get; set; }
 
-    public bool IsConfirmed { get; set; }
-  }
+        public bool Bribed { get; set; }
+
+        public bool IsConfirmed { get; set; }
+
+        public int LeaderID { get; set; }
+
+        public int VictimID { get; set; }
+
+        public int HunterID { get; set; }
+
+        public Leader Leader { get; set; }
+
+        public Leader Victim { get; set; }
+
+        public Hunter Hunter { get; set; }
+
+    }
 }
