c39a36a4e315906abc0e9642a1b4521ca8c549a0
Luis Gerardo <luisgpr99@gmail.com>
2020-12-02 14:35:04 -0600

    Create IHunterService interface

---
+++ BountyZone.Core/Interfaces/IHunterService.cs
@@ -0,0 +1,12 @@
+﻿using BountyZone.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BountyZone.Core.Interfaces
+{
+    public interface IHunterService : IBaseService<Hunter>
+    {
+        ServiceResult<IEnumerable<Bounty>> GetAvailableBounties();        
+    }
+}
