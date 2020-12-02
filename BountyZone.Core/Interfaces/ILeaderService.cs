5a2716ede8b1f599d54860571d688dedcced97e9
Luis Gerardo <luisgpr99@gmail.com>
2020-12-02 14:35:15 -0600

    Create ILeaderService interface

---
+++ BountyZone.Core/Interfaces/ILeaderService.cs
@@ -0,0 +1,14 @@
+﻿using BountyZone.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BountyZone.Core.Interfaces
+{
+    public interface ILeaderService : IBaseService<Leader>
+    {
+        ServiceResult<IEnumerable<Leader>> GetPopularVictims();
+
+        ServiceResult<string> PlaceBountyOn(int victimID);
+    }
+}
