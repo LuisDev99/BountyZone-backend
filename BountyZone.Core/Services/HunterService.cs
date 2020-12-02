4a291b3c513bdd47b9156f11d42e19d2a6b5ca54
Luis Gerardo <luisgpr99@gmail.com>
2020-12-02 16:11:50 -0600

    Create HunterService class, which implements IHunterService interface

---
+++ BountyZone.Core/Services/HunterService.cs
@@ -0,0 +1,31 @@
+﻿using BountyZone.Core.Entities;
+using BountyZone.Core.Helpers;
+using BountyZone.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BountyZone.Core.Services
+{
+    public class HunterService : BaseService<Hunter>, IHunterService
+    {
+        private readonly IRepository<Bounty> _baseBountyRepository;
+
+        public HunterService(IRepository<Hunter> repository, IRepository<Bounty> baseBountyRepository) : base(repository)
+        {
+            _baseBountyRepository = baseBountyRepository;
+        }
+
+        public ServiceResult<IEnumerable<Bounty>> GetAvailableBounties()
+        {
+            var bounties = _baseBountyRepository.Filter(bounty => !bounty.IsConfirmed && System.DateTime.UtcNow < bounty.Time);
+
+            if(Validators.IsListNullOrEmpty(bounties))
+            {
+                return ServiceResult<IEnumerable<Bounty>>.NotFoundResult("There are no victims at the moment...");
+            }
+
+            return ServiceResult<IEnumerable<Bounty>>.SuccessResult(bounties);
+        }
+    }
+}
