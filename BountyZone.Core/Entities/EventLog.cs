735bc69d94ff46a7818f5c871386b45a07f1c8d6
Luis Gerardo <luisgpr99@gmail.com>
2020-12-02 14:11:58 -0600

    Create EventLog Entity

---
+++ BountyZone.Core/Entities/EventLog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BountyZone.Core.Entities
+{
+    public class EventLog : BaseEntity
+    {
+        public int LeaderID { get; set; }
+
+        public int VictimID { get; set; }
+
+        public int HunterID { get; set; }
+
+        public Leader Leader { get; set; }
+
+        public Leader Victim { get; set; }
+
+        public Hunter Hunter { get; set; }
+    }
+}
