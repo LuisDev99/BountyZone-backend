3ded95d966408451535410c957ab0ff3cae771aa
Luis Gerardo <luisgpr99@gmail.com>
2020-12-02 13:58:32 -0600

    Create Leader Entity

---
+++ BountyZone.Core/Entities/Leader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BountyZone.Core.Entities
+{
+    public class Leader : BaseEntity
+    {
+        public int Money { get; set; }
+
+        public int Reputation { get; set; }
+
+        public int SuccessfulAttacks { get; set; }
+
+        public int SuccessfulDefends { get; set; }
+
+        public int PlayerID { get; set; }
+
+        public Player Player { get; set; }
+    }
+}
