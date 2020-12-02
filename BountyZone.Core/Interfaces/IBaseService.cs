168deeabd3bc9a7a3824037043ca13f342a1ba5e
Luis Gerardo <luisgpr99@gmail.com>
2020-12-02 14:25:23 -0600

    Create Interfaces Folder. Create IBaseService interface

---
+++ BountyZone.Core/Interfaces/IBaseService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BountyZone.Core.Interfaces
+{
+    public interface IBaseService<T>
+    {
+        ServiceResult<IEnumerable<T>> GetAll();
+
+        ServiceResult<T> GetByID(int entityID);
+
+        ServiceResult<T> Create(T data);
+
+        ServiceResult<T> Delete(int entityID);
+
+        ServiceResult<T> Update(T data);
+
+        ServiceResult<T> Patch(T data);
+    }
+}
