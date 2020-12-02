163947596af8d097cced5a6b961c0e1e2b6d5705
Luis Gerardo <luisgpr99@gmail.com>
2020-12-02 14:24:48 -0600

    Create class ServiceResult. Create enum class ResponseCode

---
+++ BountyZone.Core/Enums/ResponseCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BountyZone.Core.Enums
+{
+    public enum ResponseCode
+    {
+        Success,
+        Error,
+        ImATeaPot,
+        NotFound
+    }
+}
