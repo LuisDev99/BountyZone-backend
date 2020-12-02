10c7997e96462ba76923b21073822dccd344b8d3
Luis Gerardo <luisgpr99@gmail.com>
2020-12-02 14:01:43 -0600

    Create Hunter Entity

---
+++ BountyZone.Core/Entities/Hunter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BountyZone.Core.Entities
+{
+    public class Hunter : BaseEntity
+    {
+        public int Guns { get; set; }
+
+        public int Bribes { get; set; }
+
+        public int PlayerID { get; set; }
+
+        public Player Player { get; set; }
+    }
+}
