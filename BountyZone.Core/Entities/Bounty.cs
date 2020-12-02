d7db68e3fa74fe92666d875e3a94433aedf2d701
Luis Gerardo <luisgpr99@gmail.com>
2020-12-02 14:03:34 -0600

    Create Bounty Entity

---
+++ BountyZone.Core/Entities/Bounty.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BountyZone.Core.Entities
+{
+  public class Bounty : BaseEntity
+  {
+    public DateTime Time { get; set; }
+
+    public bool IsConfirmed { get; set; }
+  }
+}
