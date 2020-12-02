83cd15b3859e2b6b55705f314938ed7a8ae0d3ab
Luis Gerardo <luisgpr99@gmail.com>
2020-12-02 16:07:35 -0600

    Create a Helpers folder. Create a Validators static class with one method that checks if a IEnumerable of T is null or empty

---
+++ BountyZone.Core/Helpers/Validators.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BountyZone.Core.Helpers
+{
+    public static class Validators
+    {
+        public static bool IsListNullOrEmpty<T>(IEnumerable<T> list)
+        {
+            return list == null || list.Any();
+        }
+    }
+}
