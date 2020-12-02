21a1ca2f76d8c29b7702a5153e35673a0e568e54
Luis Gerardo <luisgpr99@gmail.com>
2020-12-02 14:47:31 -0600

    Create IHunterRepository and ILeaderRepository interfaces

---
+++ BountyZone.Core/Interfaces/Repositories/IHunterRepository.cs
@@ -0,0 +1,12 @@
+﻿using BountyZone.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BountyZone.Core.Interfaces.Repositories
+{
+    public interface IHunterRepository
+    {
+        IEnumerable<Bounty> GetAvailableBounties();
+    }
+}
