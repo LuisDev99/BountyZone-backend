004c397bae477fa25f4c1928296e8c2a21b888de
Luis Gerardo <luisgpr99@gmail.com>
2020-12-04 03:02:54 -0600

    Add migration for the new default values for the columns of tables Leader and Hunter

---
+++ BountyZone.Infrastructure/Migrations/20201204085839_AddDefaultValuesForHunterAndLeaderTable.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BountyZone.Infrastructure.Migrations
+{
+    public partial class AddDefaultValuesForHunterAndLeaderTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "SuccessfulAttacks",
+                table: "Leaders",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Reputation",
+                table: "Leaders",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Money",
+                table: "Leaders",
+                type: "integer",
+                nullable: false,
+                defaultValue: 1000,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Guns",
+                table: "Hunters",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Bribes",
+                table: "Hunters",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "SuccessfulAttacks",
+                table: "Leaders",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldDefaultValue: 0);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Reputation",
+                table: "Leaders",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldDefaultValue: 0);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Money",
+                table: "Leaders",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldDefaultValue: 1000);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Guns",
+                table: "Hunters",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldDefaultValue: 0);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Bribes",
+                table: "Hunters",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldDefaultValue: 0);
+        }
+    }
+}
