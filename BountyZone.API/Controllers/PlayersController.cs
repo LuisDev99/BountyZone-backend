ff4483c9d9a747cf388e08a3dd6698526eedee35
Luis Gerardo <luisgpr99@gmail.com>
2020-12-04 02:17:26 -0600

    Implement Get method for the Players controller

---
+++ BountyZone.API/Controllers/PlayersController.cs
@@ -0,0 +1,40 @@
+﻿using BountyZone.Core.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BountyZone.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayersController : ControllerBase
+    {
+        private readonly IPlayerService _playerService;
+
+        public PlayersController(IPlayerService playerService)
+        {
+            _playerService = playerService;
+        }
+
+
+        // GET api/<PlayersController>/5
+        [HttpGet]
+        public ActionResult<bool> DoesPlayerExists([FromBody] string email)
+        {
+            var service = _playerService.DoesPlayerExists(email);
+
+            return service.Result == true
+                ? Ok()
+                : NotFound();
+
+        }
+
+        // POST api/<PlayersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
