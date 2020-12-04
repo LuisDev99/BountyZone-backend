431a8d39813638b67e364b928927a1314d0166f4
Luis Gerardo <luisgpr99@gmail.com>
2020-12-04 02:20:40 -0600

    Update PlayerDTO to the latest change of the Player entity

--- BountyZone.API/Models/PlayerDTO.cs
+++ BountyZone.API/Models/PlayerDTO.cs
@@ -7,10 +7,10 @@ namespace BountyZone.API.Models
 {
     public class PlayerDTO : BaseEntity
     {
-        public string UserName { get; set; }
+        public string Email { get; set; }
 
         public string NickName { get; set; }
 
-        public string Password { get; set; }
+        public int PlayerRoleID { get; set; }
     }
 }
