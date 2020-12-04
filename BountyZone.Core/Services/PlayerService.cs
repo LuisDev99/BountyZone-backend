255135cde36f76408b18502bcfb57ef20160c3ae
Luis Gerardo <luisgpr99@gmail.com>
2020-12-04 02:14:39 -0600

    Create PlayerService class, which implements IPlayerService interface

---
+++ BountyZone.Core/Services/PlayerService.cs
@@ -0,0 +1,27 @@
+﻿using BountyZone.Core.Entities;
+using BountyZone.Core.Interfaces;
+using BountyZone.Core.Interfaces.Repositories;
+using BountyZone.Core.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BountyZone.Core.Services
+{
+    public class PlayerService :  IPlayerService
+    {
+        private readonly IPlayerRepository _playerRepository;
+
+        public PlayerService(IPlayerRepository playerRepository)
+        {
+            _playerRepository = playerRepository;
+        }
+
+        public ServiceResult<bool> DoesPlayerExists(string userEmail)
+        {
+            var player = _playerRepository.FindPlayerByEmail(userEmail);
+
+            return ServiceResult<bool>.SuccessResult(player != null);
+        }
+    }
+}
