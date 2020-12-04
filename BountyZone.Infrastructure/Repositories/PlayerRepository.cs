05ad92702e088e03349c209716336b11792d247d
Luis Gerardo <luisgpr99@gmail.com>
2020-12-04 02:16:10 -0600

    Create PlayerRepository class, which implements the IPlayerRepository interface

---
+++ BountyZone.Infrastructure/Repositories/PlayerRepository.cs
@@ -0,0 +1,21 @@
+﻿using BountyZone.Core.Entities;
+using BountyZone.Core.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BountyZone.Infrastructure.Repositories
+{
+    public class PlayerRepository : EntityFrameworkRepository<Player>, IPlayerRepository
+    {
+        public PlayerRepository(BountyZoneDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public Player FindPlayerByEmail(string email)
+        {
+            return _dbContext.Players.FirstOrDefault(player => player.Email == email);
+        }
+    }
+}
