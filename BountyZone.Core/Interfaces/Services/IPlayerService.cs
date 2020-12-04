03e03230edd4b89809e201cfa8260a925bb5a9f7
Luis Gerardo <luisgpr99@gmail.com>
2020-12-04 02:13:31 -0600

    Create IPlayerService interface for the Player entity

---
+++ BountyZone.Core/Interfaces/Services/IPlayerService.cs
@@ -0,0 +1,12 @@
+﻿using BountyZone.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BountyZone.Core.Interfaces.Services
+{
+    public interface IPlayerService
+    {
+        ServiceResult<bool> DoesPlayerExists(string userEmail);
+    }
+}
