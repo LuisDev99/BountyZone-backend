3a7a66a17fc03574328822ff7a6600a91018088e
Luis Gerardo <luisgpr99@gmail.com>
2020-12-04 03:06:39 -0600

    Implement CreatePlayerWithRole method for the IPlayerRepository interface

--- BountyZone.Infrastructure/Repositories/PlayerRepository.cs
+++ BountyZone.Infrastructure/Repositories/PlayerRepository.cs
@@ -13,6 +13,24 @@ namespace BountyZone.Infrastructure.Repositories
         {
         }
 
+        public Player CreatePlayerWithRole(Player player)
+        {
+            PlayerRole playerRole = _dbContext.PlayerRoles.Find(player.PlayerRoleID);
+
+            var newPlayer = _dbContext.Players.Add(player);
+
+            if (playerRole.Type == "Hunter")
+                _dbContext.Hunters.Add(new Hunter { PlayerID = player.ID });
+            else if (playerRole.Type == "Leader")
+                _dbContext.Leaders.Add(new Leader { PlayerID = player.ID });
+            else
+                throw new EntryPointNotFoundException("Could not determine the player role type");
+
+            _dbContext.SaveChanges();
+
+            return newPlayer.Entity;
+        }
+
         public Player FindPlayerByEmail(string email)
         {
             return _dbContext.Players.FirstOrDefault(player => player.Email == email);
