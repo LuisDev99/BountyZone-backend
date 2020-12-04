5a77ce4b7879a67596c6792fdc17ab7f77e7658c
Luis Gerardo <luisgpr99@gmail.com>
2020-12-04 02:13:03 -0600

    Remove UserName and Password properties for the Player entity. Add Email property

--- BountyZone.Core/Entities/Player.cs
+++ BountyZone.Core/Entities/Player.cs
@@ -6,11 +6,9 @@ namespace BountyZone.Core.Entities
 {
     public class Player : BaseEntity
     {
-        public string UserName { get; set; }
+        public string Email { get; set; }
 
         public string NickName { get; set; }
-
-        public string Password { get; set; }
 
         public int PlayerRoleID { get; set; }
 
