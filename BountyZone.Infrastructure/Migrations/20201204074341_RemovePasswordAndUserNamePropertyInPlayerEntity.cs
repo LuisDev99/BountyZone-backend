4dad5cd03425c1174a50a06aec07701c624a6364
Luis Gerardo <luisgpr99@gmail.com>
2020-12-04 02:15:37 -0600

    Add migrations for the new changes that occured in the Player entity class (UserName and Password properties got removed)

---
+++ BountyZone.Infrastructure/Migrations/20201204074341_RemovePasswordAndUserNamePropertyInPlayerEntity.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BountyZone.Infrastructure.Migrations
+{
+    public partial class RemovePasswordAndUserNamePropertyInPlayerEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Players");
+
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "Players",
+                newName: "Email");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Players",
+                newName: "UserName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Players",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
