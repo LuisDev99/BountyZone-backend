1d8d387d302a76d74d8c35dd6f9af3c1cc799641
Luis Gerardo <luisgpr99@gmail.com>
2020-12-04 02:14:00 -0600

    Create IPlayerRepository interface for the Player entity

---
+++ BountyZone.Core/Interfaces/Repositories/IPlayerRepository.cs
@@ -0,0 +1,12 @@
+﻿using BountyZone.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BountyZone.Core.Interfaces.Repositories
+{
+    public interface IPlayerRepository
+    {
+        Player FindPlayerByEmail(string email);
+    }
+}
