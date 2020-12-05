e3e0ba9b398ebb6084e7489e954d39d5bb06d107
Luis Gerardo <luisgpr99@gmail.com>
2020-12-04 19:48:21 -0600

    Add migrations for the new changes of PlayerRole entity class

---
+++ BountyZone.Infrastructure/Migrations/20201204225831_AddImageURLandDescriptionColumnsToPlayerRole.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BountyZone.Infrastructure.Migrations
+{
+    public partial class AddImageURLandDescriptionColumnsToPlayerRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "PlayerRoles",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageURL",
+                table: "PlayerRoles",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "PlayerRoles");
+
+            migrationBuilder.DropColumn(
+                name: "ImageURL",
+                table: "PlayerRoles");
+        }
+    }
+}
