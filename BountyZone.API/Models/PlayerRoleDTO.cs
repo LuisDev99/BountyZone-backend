e2bc93d808b8a0d2a9f7af8a3a1fcd5fad6be24e
Luis Gerardo <luisgpr99@gmail.com>
2020-12-04 19:46:02 -0600

    Create PlayerRole Data transfer object class

---
+++ BountyZone.API/Models/PlayerRoleDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BountyZone.API.Models
+{
+    public class PlayerRoleDTO
+    {
+        public int ID { get; set; }
+
+        public string Type { get; set; }
+
+        public string ImageURL { get; set; }
+
+        public string Description { get; set; }        
+    }
+}
